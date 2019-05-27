3f24e896b0dcc8c7cde5d9cd69782464c7070608
Mateus Roriz <mateusvroriz6b@gmail.com>
2019-05-26 21:34:53 -0300

    adding new responses

--- bot.js
+++ bot.js
@@ -8,15 +8,30 @@ bot.on('ready', () =>{
 })
 
 bot.on('message', msg=>{
-  if(msg.content === "JOTARO"){
+  if(msg.content === "!jotaro") ||(msg.content === "!JOTARO") {
       msg.reply("DIO!");
+      msg.reply("Oh? You’re approaching me? Instead of running away, you’re coming right to me? Even though your grandfather, Joseph, told you the secret of The World, like an exam student scrambling to finish the problems on an exam until the last moments before the chime?
+      I can’t beat the shit out of you without getting closer. Oh ho! Then come as close as you like.")
   }
-
 })
 
 bot.on('message', msg=>{
-  if(msg.content ==="uber"){
+  if(msg.content ==="!koichi"){
       msg.reply("no diginity");
+  }
+})
+
+bot.on('message', msg=>{
+  if(msg.content ==="!kira"){
+      msg.reply("My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.");
+  }
+})
+
+
+
+bot.on('message', msg=>{
+  if(msg.content ==="!uber falou bosta"){
+      msg.reply("Interessante");
   }
 
 })
