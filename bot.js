0ded7faab13cb089163c6dfa369a1d0d7c6f2117
Mateus Roriz <mateusvroriz6b@gmail.com>
2019-06-23 16:48:23 -0300

    adding bot status

--- bot.js
+++ bot.js
@@ -1,7 +1,20 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
 
-
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Ouvindo";
+    discordPresence.details = "Ram Ranch";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageText = "Numbani";
+    discordPresence.smallImageText = "JAJA- LV 0";
+    discordPresence.spectateSecret = "MTIzNDV8MTIzNDV8MTMyNDU0";
+    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+    Discord_UpdatePresence(&discordPresence);
+}
 
 bot.on('ready', () =>{
   console.log('dio');
